b26d50289ee5461aa1baca6e2ac017b354dd5292
Peter <pikruser@gmail.com>
2024-04-28 16:43:53 -0400

    notebook & helper functions

---
+++ utils/Game2Moves.py
@@ -0,0 +1,14 @@
+
+def create_move_list(s):
+
+    """
+    Given a string of moves, return a list of moves.
+
+    Parameters:
+        s (str): string of moves
+    
+    Returns
+        moves (list): list of moves
+    """
+
+    moves = re.
